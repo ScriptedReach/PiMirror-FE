4abfe2dab7f1f274f5820f61141775a6ad2f0184
Student Levi Rosheuvel <lrosheuv6991@student.landstede.nl>
2024-10-17 23:10:42 +0200

    changed some existing features and addes the Finence component to show BTC price

--- src/components/TimeAndDate.vue
+++ src/components/TimeAndDate.vue
@@ -1,27 +1,40 @@
-<template>
-  <div class="flex flex-row">
-    <div class="text-8xl">{{ currentTime }}</div>
-    <div class="text-3xl">{{ currentSeconds }}</div>
-  </div>
-</template>
-
 <script>
 export default {
   data() {
     return {
       currentTime: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }), 
       currentSeconds: new Date().getSeconds(), // Initial seconds
+      currentDate: new Date(), // Store the current date object
     };
   },
   methods: {
     updateTime() {
       const now = new Date();
-      this.currentTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
-      this.currentSeconds = now.getSeconds(); // Update seconds
+      this.currentTime = now.toLocaleTimeString();
+      // this.currentSeconds = now.getSeconds(); // Update seconds
     },
   },
   mounted() {
     this.timer = setInterval(this.updateTime, 1000); 
   },
+  computed: {
+    dateFormat() {
+      const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      const day = this.currentDate.getDate();
+      const month = monthNames[this.currentDate.getMonth()];
+      const year = this.currentDate.getFullYear();
+      return `${day} ${month} ${year}`;
+    }
+  }
 };
 </script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="flex flex-row">
+      <div class="text-8xl">{{ currentTime }}</div>
+      <!-- <div class="text-4xl ml-2">{{ currentSeconds }}</div> -->
+    </div>
+    <div class="text-3xl">{{ dateFormat }}</div>
+  </div>
+</template>
